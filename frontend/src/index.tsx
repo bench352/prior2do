7cd6656a5d931336f4f6e78d46bb7855f2dd3793
Ben Choy <97584068+bench352@users.noreply.github.com>
2022-12-24 10:18:08 +0800

    Added frontend.

---
+++ frontend/src/index.tsx
@@ -0,0 +1,34 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import MainPage from "./pages/MainPage/MainPage";
+import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,9 @@
+import BasicAppBar from "../../components/UIComponents/AppBar";
+
+export default function MainPage() {
+  return (
+    <div>
+      <BasicAppBar title="Prior2Do" />
+    </div>
+  );
+}
